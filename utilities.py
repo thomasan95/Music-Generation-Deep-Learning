70fa02828f43caac2a4937c52cb1486890a91320
Thomas <thomasan1995@gmail.com>
2018-02-26 19:42:09 -0800

    fixed???

--- utilities.py
+++ utilities.py
@@ -85,7 +85,7 @@ def val_to_tensor(val, dictionary, batch_size, labels=False):
         for seq_i in range(val_seq_len):
             tensor[seq_i, batch_i, 0] = dictionary[val[batch_i*seq_i + seq_i]]
 
-    return tensor
+    return tensor, val_seq_len
 
 
 def string_to_tensor(inp, dictionary, batch_size, seq_len, labels=False):
