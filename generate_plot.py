7e08277e527441f37b247159041be2961b3a6259
Thomas <thomasan1995@gmail.com>
2018-02-27 15:05:04 -0800

    added title

--- generate_plot.py
+++ generate_plot.py
@@ -6,10 +6,11 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file", type=str, default="", help="Path to pickle file of losses")
 parser.add_argument("-s", "--save_file", type=str, default="", help="file to save as. Will save under results")
 parser.add_argument("-uc", "--update_check", type=int, default=1000, help="how many iterations per update")
+parser.add_argument("-t", "--title", type=str, default="", help="Specify title of plot")
 args = parser.parse_args()
 
 
-def generate_plot(file, save_file=""):
+def generate_plot(file, save_file="", title=""):
     '''
     Function for visualizing training and validation loss over training cycle
 
@@ -22,6 +23,8 @@ def generate_plot(file, save_file=""):
     :type file: str
     :param save_file: if ran stand alone, it is from args.save_file, name to save the file as
     :type save_file: str
+    :param title: title of the plot to generate
+    :type title: str
     :return: None
     '''
 
@@ -43,7 +46,10 @@ def generate_plot(file, save_file=""):
         plt.plot(list(range(epochs_val)), losses['valid'], label='Valid Loss')
         plt.xlabel("Every %d Training Iterations" % args.update_check)
         plt.ylabel("Loss")
-        plt.title("Loss over Training Cycle")
+        if title == "":
+            plt.title("Loss over Training Cycle")
+        else:
+            plt.title(title)
         plt.legend(loc='upper right')
 
     else:
@@ -69,4 +75,4 @@ def generate_plot(file, save_file=""):
 
 
 if __name__ == "__main__":
-    generate_plot(args.file, args.save_file)
+    generate_plot(args.file, args.save_file, args.title)
