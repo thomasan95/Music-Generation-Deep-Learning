3a8b9250848bdee2506425d88f4966b60cb98524
Jeanette Nguyen <jeanettenguyen@Jeanettes-MacBook-Air.local>
2018-03-01 15:19:54 -0800

    tabs to spaces

--- music_generation.py
+++ music_generation.py
@@ -256,58 +256,58 @@ def heat_map(model, char2int, int2char, unit_num=args.unit_number, song_path=arg
     for song_ind in range(num_songs):
 
         generated_song = generated_songs[song_ind]
-	song_length = len(generated_song)
+        song_length = len(generated_song)
 
         tensor_song = utils.string_to_tensor([list(generated_song)], char2int , 1, song_length)
-	
+
         tensor_song = Variable(tensor_song)
         if gpu:
             tensor_song = tensor_song.cuda()
         
-	if find_special:
-	    output = model(tensor_song[0])
-            hidden, cell = model.hidden
-            hidden = hidden.data.numpy()
-	    num_units = np.shape(hidden)[2]
-	    header_correlation = -99999999
-	    body_correlation =-999999999
-	    header_filter = np.ones(song_length)
-	    header_filter[song_length/8:]=-1
-	    header_filter[:song_length/8]= 6
-	    body_filter = np.ones(song_length)
-	    body_filter[:song_length/8] = -6
-	    
-	    for unit in range(num_units):
-		activations = []
+    if find_special:
+        output = model(tensor_song[0])
+        hidden, cell = model.hidden
+        hidden = hidden.data.numpy()
+        num_units = np.shape(hidden)[2]
+        header_correlation = -99999999
+        body_correlation =-999999999
+        header_filter = np.ones(song_length)
+        header_filter[song_length/8:]=-1
+        header_filter[:song_length/8]= 6
+        body_filter = np.ones(song_length)
+        body_filter[:song_length/8] = -6
+
+        for unit in range(num_units):
+        activations = []
                 for index in range(song_length):
-		    output = model(tensor_song[index])
-            	    hidden, cell = model.hidden
+            output = model(tensor_song[index])
+                    hidden, cell = model.hidden
                     hidden = hidden.data.numpy()
-		    activations.append(hidden[0,0,unit])
-		activations = np.asarray(activations)
-		correlation = np.dot(activations,header_filter)
-		if correlation > header_correlation:
-			header_correlation = correlation
-			header = (activations,unit)
-		correlation = np.dot(activations,body_filter)
-		if correlation > body_correlation:
-			body_correlation = correlation
-			body = (activations,unit)
-	    activations = header[0]
-	    print("Header Detector for song "+str(index)+" is Unit: "+str(header[1]))
-	    print("Body Detector for song "+str(index)+" is Unit: " +str(body[1]))
-	else:
-	    activations = []
-	    for index in range(song_length):
-		#model.zero_grad()
-	    	output = model(tensor_song[index])
-		hidden, cell = model.hidden
-		cell = cell.data.numpy()
-		hidden = hidden.data.numpy()
-		    # print cell[0,0,0]
-		activations.append(hidden[0, 0, unit_num])
-	    activations = np.asarray(activations)
-	
+            activations.append(hidden[0,0,unit])
+        activations = np.asarray(activations)
+        correlation = np.dot(activations,header_filter)
+        if correlation > header_correlation:
+            header_correlation = correlation
+            header = (activations,unit)
+        correlation = np.dot(activations,body_filter)
+        if correlation > body_correlation:
+            body_correlation = correlation
+            body = (activations,unit)
+        activations = header[0]
+        print("Header Detector for song "+str(index)+" is Unit: "+str(header[1]))
+        print("Body Detector for song "+str(index)+" is Unit: " +str(body[1]))
+    else:
+        activations = []
+        for index in range(song_length):
+        #model.zero_grad()
+            output = model(tensor_song[index])
+        hidden, cell = model.hidden
+        cell = cell.data.numpy()
+        hidden = hidden.data.numpy()
+            # print cell[0,0,0]
+        activations.append(hidden[0, 0, unit_num])
+        activations = np.asarray(activations)
+
         
         print("Song Length: "+str(song_length))
         height = int(np.sqrt(song_length)) + 1
@@ -324,11 +324,11 @@ def heat_map(model, char2int, int2char, unit_num=args.unit_number, song_path=arg
         countH = height-1
         countW = 0
         for index in range(len(generated_song)):
-	    char = generated_song[index]
-	    if char == '\n':
-		char = 'nl'
-	    elif char == ' ':
-		char = 'sp'
+        char = generated_song[index]
+        if char == '\n':
+        char = 'nl'
+        elif char == ' ':
+        char = 'sp'
             plt.text(countW, countH, char)
             countW += 1
             if countW >= width:
